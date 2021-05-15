b53a9027750756d436ea08e8d0baad81e57693b1
Pradeep Manjunatha <pradeepkallis.pradeep@gmail.com>
2021-05-15 20:08:52 +0530

    After adding HTML and CSS

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Adding route filters
-app.use('/admin',adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next) => {
-    res.status(404).send("<h1>Page not found</h1>")
-})
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3000);
---
+++ helpers/path.js
@@ -0,0 +1,6 @@
+const path = require('path')
+
+
+module.exports ={ 
+    rootdir : path.dirname(process.mainModule.filename) 
+        }
